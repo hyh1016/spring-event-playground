3b97c6d669092020e35670261c916c6fc8ffce8f
YIHYUN HA <hihyun16@gmail.com>
2024-01-14 10:08:33 +0900

    Init project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Spring Event Playground
+
+스프링 내부 이벤트를 사용한 샘플 코드를 구현한 저장소입니다.
+
+틀린 내용에 대한 지적 및 피드백은 언제나 환영합니다!
+
+Author: **[@hyh1016](https://github.com/hyh1016)**
+
+사용 버전: Java 21 & Spring Boot 3.2
---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.2.1"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "yhproject.playground"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["springModulithVersion"] = "1.1.0"
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.modulith:spring-modulith-starter-core")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.modulith:spring-modulith-starter-test")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.modulith:spring-modulith-bom:${property("springModulithVersion")}")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
